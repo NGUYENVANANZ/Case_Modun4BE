2660466690b5dddaa4ac32ce1284c99e4abd5e17
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-08 22:38:37 +0700

    cm

--- src/main/java/caseModun4/config/SecurityConfig.java
+++ src/main/java/caseModun4/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/**");
-        http.authorizeRequests().antMatchers("/home", "/login", "/register").permitAll()
+        http.authorizeRequests().antMatchers( "/login", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
