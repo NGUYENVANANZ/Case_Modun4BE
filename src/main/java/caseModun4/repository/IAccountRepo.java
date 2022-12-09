a4ddd48de39254597cb08cba346ee4f5b3147b2a
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-09 08:35:06 +0700

    up

--- src/main/java/caseModun4/repository/IAccountRepo.java
+++ src/main/java/caseModun4/repository/IAccountRepo.java
@@ -2,20 +2,13 @@ package caseModun4.repository;
 
 
 import caseModun4.model.Account;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAccountRepo extends CrudRepository<Account, Long> {
-    @Query(nativeQuery = true, value = "select * from account where full_name like concat ('%',:name,'%');")
     Account findByUsername(String username);
-
 }
