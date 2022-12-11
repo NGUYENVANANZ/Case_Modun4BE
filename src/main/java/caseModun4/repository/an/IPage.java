908ae1fda85c7a67ea5e1e6ff707015211001e43
CoraSan1 <nguyendinh070199@gmail.com>
2022-12-11 22:36:40 +0700

    dinh

--- src/main/java/caseModun4/repository/an/IPage.java
+++ src/main/java/caseModun4/repository/an/IPage.java
@@ -13,10 +13,6 @@ public interface IPage extends CrudRepository<Page, Long> {
     @Query(nativeQuery = true,value = "SELECT * from page where account_id = :id and (page_status_id = 1 or page_status_id = 2)")
     List<Page> Page(@Param("id") long id);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ddfab1c74be562f03712f12dc473d373dd66f52d
     @Query(nativeQuery = true,value = "SELECT * from page where id = :id")
     Page Page1(@Param("id") long id);
 
@@ -26,10 +22,5 @@ public interface IPage extends CrudRepository<Page, Long> {
     List<Page> Page3(@Param("id") long id);
 
 
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> ddfab1c74be562f03712f12dc473d373dd66f52d
 }
