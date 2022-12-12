c027aa5d42e73fcedcab3b74f29b4e9245f3231f
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-12-12 13:52:34 +0700

    up edit profile

--- src/main/java/caseModun4/repository/manh/IPageRepo.java
+++ src/main/java/caseModun4/repository/manh/IPageRepo.java
@@ -2,7 +2,12 @@ package caseModun4.repository.manh;
 
 import caseModun4.model.Page;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IPageRepo extends CrudRepository<Page,Long> {
 
+    Page findPageById(long id);
+
+//    Page deletePageById(long id);
 }
--- src/main/java/caseModun4/service/manh/ProfileService.java
+++ src/main/java/caseModun4/service/manh/ProfileService.java
@@ -4,15 +4,13 @@ import caseModun4.model.Account;
 import caseModun4.model.Page;
 import caseModun4.repository.IAccountRepo;
 import caseModun4.repository.an.IPage;
+import caseModun4.repository.manh.IPageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -24,6 +22,9 @@ public class ProfileService {
   @Autowired
   IPage iPage;
 
+  @Autowired
+  IPageRepo iPageRepo;
+
 
   public Account findaccountsbyname (String username){
     return iAccountRepo.findByUsername(username);
@@ -32,5 +33,15 @@ public class ProfileService {
   public List<Page> pageList(long id){
     return (List<Page>) iPage.Page2(id);
   }
+
+
+  public Page findPageById(long id){
+    return iPageRepo.findPageById(id);
+  }
+
+  public void deletePageById(long id){
+    iPageRepo.deleteById(id);
+  }
+
 
 }
