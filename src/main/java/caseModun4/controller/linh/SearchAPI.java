ac4679894b7f3805bffb8017f14c5f9b8abaf576
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-12 08:56:18 +0700

    up

--- src/main/java/caseModun4/controller/linh/SearchAPI.java
+++ src/main/java/caseModun4/controller/linh/SearchAPI.java
@@ -1,7 +1,9 @@
 package caseModun4.controller.linh;
 
 import caseModun4.model.Account;
+import caseModun4.model.Page;
 import caseModun4.service.linh.IAccountServiceSearch;
+import caseModun4.service.linh.IPageServiceSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +19,21 @@ public class SearchAPI {
     @Autowired
     IAccountServiceSearch iAccountServiceSearch;
 
+    @Autowired
+    IPageServiceSearch iPageServiceSearch;
+
     @GetMapping("/{name}")
     public ResponseEntity<List<Account>> findByName(@PathVariable String name) {
         return new ResponseEntity<>(iAccountServiceSearch.findByName(name), HttpStatus.OK);
     }
+
+//    @GetMapping("/#{text}")
+//    public ResponseEntity<List<Page>> findByText(@PathVariable String text) {
+//        return new ResponseEntity<>(iPageServiceSearch.findByText(text), HttpStatus.OK);
+//    }
 //    @GetMapping("")
 //    public ResponseEntity<List<Account>> getAll() {
 //        return new ResponseEntity<>(iAccountServiceSearch.getAll(), HttpStatus.OK);
 //    }
-
-
-
 }
 
