d47a598d095ed369166d62e1ff0b98d5eef63f96
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-12-13 11:59:29 +0700

    up

--- src/main/java/caseModun4/controller/manh/ProfileAPI.java
+++ src/main/java/caseModun4/controller/manh/ProfileAPI.java
@@ -1,14 +1,18 @@
 package caseModun4.controller.manh;
 
 import caseModun4.model.Account;
+import caseModun4.model.Friend;
 import caseModun4.model.Page;
 import caseModun4.model.PageStatus;
 import caseModun4.model.dto.PageDTO;
 import caseModun4.repository.IAccountRepo;
 import caseModun4.repository.an.IPage;
 import caseModun4.repository.an.IPageStatus;
+import caseModun4.repository.manh.IFriendRepo;
+import caseModun4.service.AccountService;
 import caseModun4.service.JwtService;
 import caseModun4.service.an.AnService;
+import caseModun4.service.manh.FriendService;
 import caseModun4.service.manh.IFriendService;
 import caseModun4.service.manh.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,25 +25,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping(path = "/profiles")
 public class ProfileAPI {
-  @Autowired
-  AuthenticationManager authenticationManager;
+
 
   @Autowired
-  JwtService jwtService;
+  IFriendRepo iFriendRepo;
 
   @Autowired
-  IAccountRepo iAccountRepo;
-
+  AccountService accountService;
   @Autowired
   ProfileService profileService;
-  @Autowired
-  IFriendService iFriendService;
 
   @Autowired
   IPageStatus iPageStatus;
@@ -49,24 +50,24 @@ public class ProfileAPI {
 
 
   @GetMapping("/profile")
-  public  ResponseEntity<Account> findById() {
+  public ResponseEntity<Account> findById() {
     UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     Account account = profileService.findaccountsbyname(userDetails.getUsername());
-    return new ResponseEntity<>(account ,HttpStatus.OK);
+    return new ResponseEntity<>(account, HttpStatus.OK);
   }
 
 
   @GetMapping("/pageProfile")
-  public  ResponseEntity<List<Page>> postProfile() {
+  public ResponseEntity<List<Page>> postProfile() {
     UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     Account account = profileService.findaccountsbyname(userDetails.getUsername());
     List<Page> page = profileService.pageList(account.getId());
-    return new ResponseEntity<>(page ,HttpStatus.OK);
+    return new ResponseEntity<>(page, HttpStatus.OK);
   }
 
 
   @PostMapping("/post")
-  public ResponseEntity<Page> checkUser(@RequestBody Page page ){
+  public ResponseEntity<Page> checkUser(@RequestBody Page page) {
     UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     Account account = profileService.findaccountsbyname(userDetails.getUsername());
     Page page1 = page;
@@ -75,23 +76,23 @@ public class ProfileAPI {
     page1.setCmts(null);
     page1.setLikePages(null);
     iPage.save(page);
-    return new ResponseEntity<>(page ,HttpStatus.OK);
+    return new ResponseEntity<>(page, HttpStatus.OK);
   }
 
   @GetMapping("/deletepost/{id}")
-  public ResponseEntity<Page> delete (@PathVariable long id){
+  public ResponseEntity<Page> delete(@PathVariable long id) {
     profileService.deletePageById(id);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
   @GetMapping("/geteditpost/{id}")
-  public ResponseEntity<Page> getPage(@PathVariable long id){
-    Page page =profileService.findPageById(id);
-    return new ResponseEntity<>(page,HttpStatus.OK);
+  public ResponseEntity<Page> getPage(@PathVariable long id) {
+    Page page = profileService.findPageById(id);
+    return new ResponseEntity<>(page, HttpStatus.OK);
   }
 
   @PostMapping("/edit/{id}&{idStatus}")
-  public ResponseEntity<Page> edit(@RequestBody PageDTO page2, @PathVariable long id, @PathVariable long idStatus){
+  public ResponseEntity<Page> edit(@RequestBody PageDTO page2, @PathVariable long id, @PathVariable long idStatus) {
     Page page1 = profileService.findPageById(id);
     PageStatus pageStatus = iPageStatus.findById(idStatus).get();
 
@@ -101,6 +102,24 @@ public class ProfileAPI {
     page1.setPageStatus(pageStatus);
 
     iPage.save(page1);
-    return new ResponseEntity<>(page1,HttpStatus.OK);
+    return new ResponseEntity<>(page1, HttpStatus.OK);
   }
+
+  @GetMapping("/friendProfile")
+  public ResponseEntity<List<Friend>> friends() {
+    UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    Account account = profileService.findaccountsbyname(userDetails.getUsername());
+    List<Friend> friends = iFriendRepo.findAllById(account.getId());
+    List<Friend> friends1 = new ArrayList<>();
+    for (int i = 0; i < friends.size(); i++) {
+      if (friends.get(i).getFriendStatus().getId()==1){
+        friends1.add(friends.get(i));
+      }
+    }
+    return new ResponseEntity<>(friends1, HttpStatus.OK);
+  }
+
+
+
+
 }
