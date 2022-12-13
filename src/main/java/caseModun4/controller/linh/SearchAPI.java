c1e630b63cdbb3753dd9de6a56bfa37729ae838e
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-13 13:58:11 +0700

    linh

--- src/main/java/caseModun4/controller/linh/SearchAPI.java
+++ src/main/java/caseModun4/controller/linh/SearchAPI.java
@@ -4,9 +4,12 @@ import caseModun4.model.Account;
 import caseModun4.model.Page;
 import caseModun4.service.linh.IAccountServiceSearch;
 import caseModun4.service.linh.IPageServiceSearch;
+import caseModun4.service.linh.ShowPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +25,9 @@ public class SearchAPI {
     @Autowired
     IPageServiceSearch iPageServiceSearch;
 
+    @Autowired
+    ShowPageService showPageService;
+
     @GetMapping("/{name}")
     public ResponseEntity<List<Account>> findByName(@PathVariable String name) {
         return new ResponseEntity<>(iAccountServiceSearch.findByName(name), HttpStatus.OK);
@@ -31,6 +37,12 @@ public class SearchAPI {
     public ResponseEntity<List<Page>> findByText(@PathVariable String text) {
         List<Page> pages = iPageServiceSearch.findByText(text);
         return new ResponseEntity<>(pages, HttpStatus.OK);
+    }
+
+    @GetMapping("/showPageText/{id}")
+    public ResponseEntity<Page> showPageText(@PathVariable Long id){
+        Page page = showPageService.findPageById(id);
+        return new ResponseEntity<>(page,HttpStatus.OK);
     }
 //    @GetMapping("")
 //    public ResponseEntity<List<Account>> getAll() {
