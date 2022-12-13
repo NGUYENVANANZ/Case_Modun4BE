7e36b8c339a57f5cda9ea4b5eb9ee14a96cbb717
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-12-13 12:13:17 +0700

    up

--- src/main/java/caseModun4/repository/manh/IPageRepo.java
+++ src/main/java/caseModun4/repository/manh/IPageRepo.java
@@ -1,13 +1,21 @@
 package caseModun4.repository.manh;
 
+import caseModun4.model.Friend;
 import caseModun4.model.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IPageRepo extends CrudRepository<Page,Long> {
 
     Page findPageById(long id);
+
+
+    @Query(nativeQuery = true,value = "SELECT * from friend where account_id = :id")
+    List<Friend> findAllById(long id);
 
 //    Page deletePageById(long id);
 }
